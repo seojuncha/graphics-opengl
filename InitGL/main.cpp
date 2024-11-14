7c1ef6e28137467c095697c43d6f1289685a3ad6
Seojun <sjcha36@gmail.com>
2024-11-14 17:00:06 +0900

    add docs with title sections and start with basic concepts

--- InitGL/main.cpp
+++ InitGL/main.cpp
@@ -3,8 +3,120 @@
 
 #include <iostream>
 
-void FrameBufferSizeCallback(GLFWwindow* window, int width, int height) {
+const char* vertexShaderSource =
+"#version 460 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main() {\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 460 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
+unsigned int vertexShader = 0;
+unsigned int fragmentShader = 0;
+unsigned int shaderProgram = 0;
+
+unsigned int vbo = 0;
+unsigned int vao = 0;
+
+bool ErrorInCompileShader(GLuint shader)
+{
+    int success;
+    char log[512];
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (success) return true;
+
+    glGetShaderInfoLog(shader, 512, NULL, log);
+    std::cout << "error shader compile: " << log << std::endl;
+
+    return false;
+}
+
+void CompileVertextShader()
+{
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    ErrorInCompileShader(vertexShader);
+}
+
+void CompileFragmentShader()
+{
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    ErrorInCompileShader(fragmentShader);
+}
+
+void CreateShaderProgram()
+{
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+void VertexLinking()
+{
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+void CreateVBO()
+{
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(vbo, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void CreateVAO()
+{
+    glGenVertexArrays(1, &vao);
+}
+
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height)
+{
     glViewport(0, 0, width, height);
+}
+
+void Initialize()
+{
+    CompileVertextShader();
+    CompileFragmentShader();
+    CreateShaderProgram();
+}
+
+void Cleanup()
+{
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+// Normalize
+GLfloat N_RGBA(float value)
+{
+    return (value / 255.0f);
 }
 
 int main()
@@ -14,7 +126,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(400, 400, "opengl test", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "opengl test", NULL, NULL);
     if (!window) {
         std::cout << "failed to create window";
         glfwTerminate();
@@ -26,10 +138,15 @@ int main()
         return 1;
     }
 
-    glViewport(0, 0, 400, 400);
+    glViewport(0, 0, 600, 600);
     glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
-
+    
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(N_RGBA(79.0f), N_RGBA(58.0f), N_RGBA(69.0f), 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Drawing...
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # graphics-opengl
+I found that a lot of C/C++ works are related with a graphic system. 
+
+
+## Why OpenGL?
+Simple thing, by now. Moreover, I don't like a os-specific, closed platform.
+
+## Environment Setup
+
+### Getting Started with VS2022
+
+### GLFW and GLAD
+OpenGL is not a library itself but a specification like an API.
+
+### Create a window
+object, context, 
+
+### Graphics Pipeline
+3D(OpenGl) -> 2D(screen)
+
+3D coordinate(x, y, z), primitives, vertex(collection of vecticies), shader
+
+range (-1.0<=x<=1.0), vertex buffer object(vbo), graphic memory, 
+
+vertex buffer object, vertex array object
+
+#### Why traiangle?
+
+## TODO
+- [] Make it run on macOS and Linux by using CMake
+
+
+## Resources
